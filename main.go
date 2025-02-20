292cfb5761a71b0a0006178f1e7cd6b23d34588c
Joshua <josh@eversoles.org>
2025-02-20 18:40:41 +0800

    arrays, slices, maps

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go!")
-	fmt.Println("Test #2. please commit")
-	var numerator int = 11
-	var denominator int = 2
-	var result, remainder int = intDivision(numerator, denominator)
-	fmt.Println("The result of dividing", numerator, "by", denominator, "is", result, "with a remainder of", remainder)
+	printMe("Hello, World!")
+	var result, remainder = intDivision(5, 2)
+	fmt.Println("The result is", result)
+	fmt.Println("The remainder is", remainder)
+	arrays()
+	slices()
+	maps()
+
 }
 
 func printMe(printValue string) {
@@ -19,4 +21,41 @@ func intDivision(numerator int, denominator int) (int, int) {
 	var result int = numerator / denominator
 	var remainder int = numerator % denominator
 	return result, remainder
+}
+
+funct arrays(){
+	var intArr [3]int32 //can't change the length of an array once it's declared
+	intArr[0] = 1
+	fmt.Println(intArr[1:3])
+}
+
+funct slices(){
+	//slices are like arrays but can change in size
+	var intSlice []int32
+	intSlice = append(intSlice, 1)
+	fmt.Println("The length of intSlice is", len(intSlice))
+	intSlice = append(intSlice, 2)
+	intSlice = append(intSlice, 3)
+	fmt.Println("The length of intSlice is", len(intSlice))
+	fmt.Println(intSlice)
+	var intSlice2 []int32 = make(int32[], 3, 8)
+}
+
+func maps(){
+	//maps are like dictionaries in python
+	var intMap map[string]int32 = make(map[string]int32)
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	fmt.Println(intMap)
+
+	var map2 = map[string]uint8{"Mickey": 15, "Minnie": 20}
+	fmt.Println(map2["Minnie"])
+	var age, ok = map2["Goofy"]
+	if ok{
+		fmt.Println("Goofy's age is", age)
+	}
+	else{
+		fmt.Println("Goofy is not in the map")
+	}
 }
